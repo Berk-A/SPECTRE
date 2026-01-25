e54dc84e016d8d797c58a8d9b088bbff6aebf570
Berk-A <alemdarberk@hotmail.com>
2026-01-25 18:49:07 +0100

    tertwe

---
+++ spectre_protocol/calc_discriminator_more.js
@@ -0,0 +1,28 @@
+
+const crypto = require('crypto');
+
+function getDiscriminator(name) {
+    const preimage = `global:${name}`;
+    const hash = crypto.createHash('sha256').update(preimage).digest();
+    return hash.subarray(0, 8);
+}
+
+const candidates = [
+    'deposit',
+    'transact',
+    'transact_spl',
+    'shield',
+    'insert_leaf',
+    'insert_leaves',
+    'append_leaf',
+    'append_leaves',
+    'process_transaction',
+    'top_up',
+    'fund_tree',
+    'new_account',
+    'create_account'
+];
+
+candidates.forEach(name => {
+    console.log(`${name}: [${Array.from(getDiscriminator(name)).join(', ')}]`);
+});
